b771bcc55ebea5d5f217785b5ec291e2b225c33c
uday0404G <udaylashkari2@gmail.com>
2024-06-06 12:52:02 +0530

    login signup using array store values in array of object and use sweet alert for alert

---
+++ loginsignup/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Loginsignup from './component/Loginsignup';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Loginsignup/>
+    </div>
+  );
+}
+
+export default App;
---
+++ loginsignup/src/component/Loginsignup.jsx
@@ -0,0 +1,145 @@
+import React, { useState } from 'react'
+// import '../App.css'
+import Swal from 'sweetalert2'
+
+function Loginsignup() {
+    let [state,setState] = useState('Login');
+    let [name,setName] = useState("");
+    let [email,setEmail] = useState("");
+    let [password,setPassword] = useState("");
+    let [arr,setarr] = useState([]);
+    let [lemail,setLemail] = useState("");
+    let [lpassword,setLpassword] = useState("");
+
+        function signup(){
+            let obj={
+                name:name,
+                email:email,
+                password:password
+            }
+            setarr([...arr,obj]);
+            console.log(arr);
+        }
+        function Login(){
+            let data = arr.filter((el)=>el.email== lemail && el.password==lpassword)
+            if(data.length>0){
+                Swal.fire({
+                    title: "LOGIN SUCCSESSFULL!",
+                    text: "Thank You!",
+                    icon: "success"
+                  });
+                
+            }
+            else{
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Sign up Now!",
+                    });
+                setState("signup")
+              }
+        }
+
+  return (
+    <div>
+    {state === 'Login' ? 
+    
+        <body>
+    
+    <section> 
+        <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
+
+  <div className="signin"> 
+
+   <div className="content"> 
+
+    <h2>Login</h2> 
+
+    <div className="form"> 
+
+     <div className="inputBox"> 
+
+<input type="email" required onChange={(el)=>setLemail(el.target.value)?"sucsess":"not"}/> <i>Email</i> 
+
+</div> 
+     <div className="inputBox"> 
+
+      <input type="password" required onChange={(el)=>setLpassword(el.target.value)}/> <i>Password</i> 
+
+     </div> 
+
+     <div className="links"> <a href="#">Forgot Password</a>
+      <a href='#' onClick={()=>setState("SignUp")}>Sign UP</a>
+     </div> 
+
+     <div className="inputBox"> 
+
+      <input type="submit" value="Login" onClick={Login}/> 
+
+     </div> 
+
+    </div> 
+
+   </div> 
+
+  </div> 
+
+ </section>
+   </body>
+     
+     : 
+       <body>
+    
+      <section> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> 
+  
+    <div className="signin"> 
+  
+     <div className="content"> 
+  
+      <h2>Sign Up</h2> 
+  
+      <div className="form"> 
+  
+       <div className="inputBox"> 
+  
+        <input type="text" required onChange={(el)=>setName(el.target.value)}/> <i>Username</i> 
+  
+       </div> 
+       <div className="inputBox"> 
+  
+  <input type="email" required onChange={(el)=>setEmail(el.target.value)}/> <i>Email</i> 
+  
+  </div> 
+       <div className="inputBox"> 
+  
+        <input type="password" required onChange={(el)=>setPassword(el.target.value)}/> <i>Password</i> 
+  
+       </div> 
+  
+       <div className="links"> <a href="#">Forgot Password</a> 
+       <a href="#" onClick={()=>setState("Login")}>Login</a> 
+  
+       </div> 
+  
+       <div className
+="inputBox"> 
+  
+        <input type="submit" value="Login" onClick={signup} /> 
+  
+       </div> 
+  
+      </div> 
+  
+     </div> 
+  
+    </div> 
+  
+   </section>
+     </body>
+    }
+  </div>
+    
+  )
+}
+
+export default Loginsignup
