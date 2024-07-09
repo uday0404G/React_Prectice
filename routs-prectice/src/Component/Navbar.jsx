3e5d01ca16fa544b6aedd04f3c01eb5dcbf5469f
Uday0404G <udaylashkari2@gmai.com>
2024-07-09 17:34:31 +0530

    Routs created

--- routs-prectice/src/Component/Navbar.jsx
+++ routs-prectice/src/Component/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
   return (
-    <div>Navbar</div>
-  )
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand >React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+           <NavLink  to={"/"} > <Navbar.Brand> Home</Navbar.Brand></NavLink>
+           <NavLink  to={"/men"} > <Navbar.Brand>Man</Navbar.Brand></NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Navbar
+export default BasicExample;
--- routs-prectice/src/Routs/Rout.jsx
+++ routs-prectice/src/Routs/Rout.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Public/Home'
+import Man from '../Public/Man'
+import Women from '../Public/Women'
+import Singlepage from '../Public/Singlepage'
+
+
 
 const Rout = () => {
   return (
-    <div>Rout</div>
+<Routes>
+  <Route path="/" element={<Home/>} ></Route>
+  <Route path="/men" element={<Man />} ></Route>
+  <Route path="/Women" element={<Women />} ></Route>
+  <Route path="/Singlepage/:id" element={<Singlepage />} ></Route>
+
+</Routes>
+
   )
 }
 
 export default Rout                            
-+
