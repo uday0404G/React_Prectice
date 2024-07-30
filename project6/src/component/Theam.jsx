7a6265f9aae4c242aec8cb0e8731b05632b3ccf0
uday0404G <udaylashkari2@gmail.com>
2024-07-30 12:43:52 +0530

    Dfd

--- project6/src/component/Theam.jsx
+++ project6/src/component/Theam.jsx
@@ -26,27 +26,41 @@ const Theam = () => {
 
 
     },[Theam])
+   
   return (
     <>
     
-    <select onChange={HandallangChange}>
-      <option value="">Select Lengauge</option>
+    <div className="container">
+      <div className='sub-lcontainer'>
 
-        <option value="English" >English</option>
-        <option value="Hindi">Hindi</option>
-        <option value="Bhojpuri">Bhojpuri</option>
-    </select>
-    
-    <select onChange={HandalChange}>
-         <option value="">Select Them</option>
-        <option value="Light">Light</option>
-        <option value="Dark">Dark</option>
-    </select>
+            <select onChange={HandallangChange} value={lang}>
+                <option value="">Select Language</option>
+                <option value="English">English</option>
+                <option value="Hindi">Hindi</option>
+                <option value="Bhojpuri">Bhojpuri</option>
+            </select>
+
+            <select onChange={HandalChange} value={Theam}>
+                <option value="">Select Theme</option>
+                <option value="Light">Light</option>
+                <option value="Dark">Dark</option>
+            </select>
+      </div>
+      <div className='lang'>
+        
+
   {
-    lang === "Bhojpuri" ? <h1>Bhojpuri</h1> : lang === "Hindi" ? <h1>Hindi</h1> : <h1>English</h1>
-
+    lang === "Bhojpuri" ? <iframe width="800" height="500" src="https://www.youtube.com/embed/_6chSkawSXw?si=Z-GOge0jAzflS4FU" title="YouTube video player" ></iframe>:
+     lang === "Hindi" ? 
+     <iframe width="800" height="500" src="https://www.youtube.com/embed/Nnop2walGmM?si=Z74eQjufFGssYU1G"
+      title="YouTube video player"  
+      ></iframe>
+     : <iframe width="800" height="500" src="https://www.youtube.com/embed/T3E9Wjbq44E?si=1CTMwbEqcCQz6PVI" title="YouTube video player" ></iframe>
+    
   }
-
+  </div>
+  </div>
+  
     </>
   )
 }
